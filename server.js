fc68a721ba05d7ef31713d36fd5f6752d6b27047
lilawren <li.lawrly@gmail.com>
2017-12-13 21:11:08 -0800

    basic server setup for sessions w/ mongodb

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/authDb');
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('DB CONNECTED');
+    // we're connected!
+});
+
+// //use sessions for tracking logins
+app.use(session({
+    secret: 'magical koolaid here',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: db
+    })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var routes = require('./routes/router');
 app.use('/', routes);
@@ -7,6 +36,22 @@ app.use('/', routes);
 // serve static files from /client
 app.use(express.static(__dirname + '/client'));
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
+
+
+
 app.listen(8000, function () {
-  console.log('Express app listening on port 8000');
+    console.log('Express app listening on port 8000');
 });
